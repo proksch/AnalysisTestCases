d738d925200c4088db1da6c420fe158b75c170a2
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2015-10-07 15:15:49 +0200

    added some test cases for switch

---
+++ Inlining/BasicStructures/Switch/DefaultCase.cs
@@ -0,0 +1,63 @@
+﻿namespace Inlining.BasicStructures.Switch
+{
+    internal class DefaultCase
+    {
+        public enum E
+        {
+            A,
+            B,
+            C
+        }
+
+        public void EmptySwitch()
+        {
+            switch ()
+            {
+            }
+        }
+
+        public void OnEnum(E e)
+        {
+            switch (e)
+            {
+                case E.A:
+                    break;
+            }
+        }
+
+        public void OnInt(int i)
+        {
+            switch (i)
+            {
+                case 2:
+                    break;
+            }
+        }
+
+        public void MultipleSections(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    Equals(null);
+                    break;
+                case 2:
+                    GetHashCode();
+                    break;
+            }
+        }
+
+        public void WithDefaultSection(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    Equals(null);
+                    break;
+                default:
+                    GetHashCode();
+                    break;
+            }
+        }
+    }
+}
