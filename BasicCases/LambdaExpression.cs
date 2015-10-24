04e54c1804e5695521b09e9d2f12536da06174f8
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2015-10-25 01:16:12 +0200

    added some tests for lambda expressions

---
+++ BasicCases/LambdaExpression.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace BasicCases
+{
+    internal class LambdaExpression
+    {
+        public void Regular()
+        {
+            Func<int, int> f = i => { return i + 1; };
+        }
+
+        public void ExpressionStyle()
+        {
+            Func<int, int> f = i => i + 1;
+        }
+
+        public void AnonymousMethod()
+        {
+            Func<int, int> f = delegate { return 1; };
+        }
+
+        public void LambdaInvocation_Implicit(Action<int> f)
+        {
+            f(1);
+        }
+
+        public void LambdaInvocation_Explicit(Action<int> f)
+        {
+            f.Invoke(1);
+        }
+
+        private Action<int> _f;
+        public void LambdaInvocation_Field()
+        {
+            _f(1);
+            
+        }
+
+        public void LambdaInLambda()
+        {
+            Func<int, int> f = i =>
+            {
+                Func<int, int> f2 = i2 => i2 + 1;
+                return f2(i);
+            };
+        }
+
+        public void MethodInLambda()
+        {
+            Func<int, int> f = i => { return M(); };
+        }
+
+        public int M()
+        {
+            return 1;
+        }
+
+        public void Assignment()
+        {
+            Func<int> f = M;
+        }
+
+        public void LocalVariable()
+        {
+            Action<int> a = null;
+            a(1);
+        }
+    }
+}
