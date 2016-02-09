3c3dde3f7c5e228c33ad9cb548553e60a16d1315
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-02-09 17:10:14 +0100

    adde some examples to test SST printing

---
+++ Printing/BinaryExpressions.cs
@@ -0,0 +1,44 @@
+﻿namespace Printing
+{
+    internal class BinaryExpressions
+    {
+        private bool B1 { get; set; }
+        private bool B2 { get; set; }
+
+        private int I1 { get; set; }
+        private int I2 { get; set; }
+
+        public void Logial()
+        {
+            bool b;
+            b = I1 < I2;
+            b = I1 <= I2;
+            b = I1 == I2;
+            b = I1 >= I2;
+            b = I1 > I2;
+            b = I1 != I2;
+            b = B1 && B2;
+            b = B1 || B2;
+        }
+
+        public void Arithmetic()
+        {
+            int i;
+            i = I1 + I2;
+            i = I1 - I2;
+            i = I1*I2;
+            i = I1/I2;
+            i = I1%I2;
+        }
+
+        public void Bitwise()
+        {
+            int i;
+            i = I1 & I2;
+            i = I1 | I2;
+            i = I1 ^ I2;
+            i = I1 << I2;
+            i = I1 >> I2;
+        }
+    }
+}
---
+++ Printing/UnaryExpressions.cs
@@ -0,0 +1,32 @@
+﻿namespace Printing
+{
+    internal class UnaryExpressions
+    {
+        private bool B1 { get; set; }
+        private bool B2 { get; set; }
+
+        private int I1 { get; set; }
+        private int I2 { get; set; }
+
+        public void Logical()
+        {
+            var b = !B1;
+        }
+
+        public void Arithmetic()
+        {
+            int i;
+            i = ++I1;
+            i = I1++;
+            i = --I1;
+            i = I1--;
+            i = -I1;
+            i = +I1;
+        }
+
+        public void Bitwise()
+        {
+            var b = ~I1;
+        }
+    }
+}
