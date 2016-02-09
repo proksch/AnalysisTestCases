dc08672e367dd773c89f203fa67de1217067516e
Sebastian Proksch <proksch@st.informatik.tu-darmstadt.de>
2016-02-09 16:50:12 +0100

    added examples for declaring constants

---
+++ BasicCases/ConstDeclarations.cs
@@ -0,0 +1,13 @@
+﻿namespace BasicCases
+{
+    internal class ConstDeclarations
+    {
+        private const int ConstField = 1;
+
+        private void M()
+        {
+            const int constVar = 2;
+            var i = 3;
+        }
+    }
+}
